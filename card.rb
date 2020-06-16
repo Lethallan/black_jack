a426be235c4c4f938db3075e01b5517824fec587
Daria Korytova <dk17march@gmail.com>
2020-06-16 17:31:50 +0300

    black jack

--- card.rb
+++ card.rb
@@ -8,9 +8,21 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    @points = POINTS_HASH.fetch(value)
+    @points = POINTS_HASH.fetch(@value)
   end
 end
 
  #'A' is not in hash, it should be checked somewhere else
  #Fetch doesn't work... bullshit
+
+#  9: from /usr/bin/irb:23:in `<main>'
+#  8: from /usr/bin/irb:23:in `load'
+#  7: from /Library/Ruby/Gems/2.6.0/gems/irb-1.0.0/exe/irb:11:in `<top (required)>'
+#  6: from (irb):31
+#  5: from (irb):12:in `new_deck'
+#  4: from (irb):12:in `each'
+#  3: from (irb):13:in `block in new_deck'
+#  2: from (irb):13:in `each'
+#  1: from (irb):14:in `block (2 levels) in new_deck'
+# NameError (uninitialized constant Deck::Card)
+# irb(main):032:0>
